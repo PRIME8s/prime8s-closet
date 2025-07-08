74e68f04d741764392e2760d8fba33860b9015d1
PRIME8s <prime8sape@protonmail.com>
2025-07-07 22:10:33 -0700

    Update script.js

--- script.js
+++ script.js
@@ -23,8 +23,8 @@ function drawCanvas() {
 }
 
 function loadByTokenId(tokenId) {
-  const paddedId = tokenId.padStart(3, '0');
-  const imageUrl = `assets/prime8s/${paddedId}.png`;
+  const paddedId = tokenId.padStart(0, '0');
+  const imageUrl = `assets/prime8s/${paddedId}.webp`;
 
   baseImage = new Image();
   baseImage.crossOrigin = 'anonymous';
